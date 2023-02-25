89591b1f724fdc49eebfce8845e9f9eb233e4fcc
olegoofy <martynenkooleg77@gmail.com>
2023-02-25 20:39:06 +0200

    Import service

--- server.go
+++ server.go
@@ -2,10 +2,12 @@ package main
 
 import (
     "net/http"
+    "labik-1/service"
 )
 
 func main() {
-    
+    timeService := service.NewTimeService()
+
     http.HandleFunc("/time", timeService.GetTimeHandler)
 
     http.ListenAndServe(":8080", nil)
